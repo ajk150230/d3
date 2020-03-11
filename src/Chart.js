84b63560bf20efcc6b5cc8c5895bd01ea3368c55
Alex <sungkim469@gmail.com>
2020-03-11 14:43:07 -0500

    errors

--- src/Chart.js
+++ src/Chart.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3'
-const data =[1,2,3,]
 class Chart extends Component {
     componentDidMount(){
-        d3.select(this.refs.temperatures)
+        const data =[1,2,3]
+        d3.select(this.refs.temp)
         .selectAll("h2")
-        .data(data)
-        .enter()
-            .append("h2")
-            .text("New Temperature")
+        this.draw(data)
+    }
+    draw(data){
+        const tempHeight = 400;
+        const tempWidth = 500;
+        const scale = 20;
+        const svgTemp = d3.select(this.refs.temp)
+            .append("svg")
+            .attr("width", tempWidth)
+            .attr("height", tempHeight)
+        svgTemp.selectAll("rectangle")
+            .data(data).enter()
+            .append("rectangle")
+            .attr("width", 30)
+            .attr("height", (e)=> e * scale)
+            .attr("fill", "red")
+            .attr('x', (e, i) => i * 45 )
+            .attr('y', (e) => tempHeight - e * scale )
+            .text(e=>e)
     }
     render() {
         return (
-            <div ref="temperatures">
+            <div ref="temp">
                 
             </div>
         );
